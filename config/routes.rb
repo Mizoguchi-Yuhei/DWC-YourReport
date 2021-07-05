eca93019ae8f2c128be269f6a65d295e5d7b594e
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-07-05 15:00:27 +0000

    observers model, controller, views追加 routesは途中

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
 
+# 学習者ログイン
   # devise_for :learners
   devise_for :learners, :controllers => {
     :registrations => 'learners/registrations',
@@ -10,6 +11,18 @@ Rails.application.routes.draw do
 
   devise_scope :learner do
     get "sign_in", :to => "learners/sessions#new"
+    post "sign_in", :to => "learners/sessions#create"
     get "sign_out", :to => "learners/sessions#destroy"
   end
+
+
+# 保護者ログイン
+  devise_for :observers
+
+  namespace :observer do
+    resources :learners, only: [:show] do
+      resource :follow_requests, only: [:create, :destroy]
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_05_131121) do
+ActiveRecord::Schema.define(version: 2021_07_05_145137) do
 
   create_table "follow_requests", force: :cascade do |t|
     t.integer "learner_id"
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 2021_07_05_131121) do
     t.integer "observer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "observers", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_observers_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_observers_on_reset_password_token", unique: true
   end
 
   create_table "subjects", force: :cascade do |t|
