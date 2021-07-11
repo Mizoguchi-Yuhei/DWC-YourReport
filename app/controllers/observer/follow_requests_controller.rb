c09cbc4a35f7ee46c409db94a6bbb11b787ddbbf
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-07-11 06:04:45 +0000

    フォロー解除機能

--- app/controllers/observer/follow_requests_controller.rb
+++ app/controllers/observer/follow_requests_controller.rb
@@ -3,13 +3,13 @@ class Observer::FollowRequestsController < ApplicationController
 		learner = Learner.find(params[:learner_id])
 		request = current_observer.follow_requests.new(learner_id: learner.id, observer_id: current_observer.id)
 		request.save
-		redirect_to observer_mypage_path(current_observer)
+		redirect_to observer_search_path
 	end
 
 	def destroy
-		learner = Learner.find(parsms[:learner_id])
+		learner = Learner.find(params[:learner_id])
 		request = current_observer.follow_requests.find_by(learner_id: learner.id, observer_id: current_observer.id)
-		request.dastroy
-		redirect_to observer_mypage_path(current_observer)
+		request.destroy
+		redirect_to observer_search_path
 	end
 end
